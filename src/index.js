cee8c6da202fc744c4f7b00dfeb52bed0edc03f9
Pyry Kröger <pyry.kroger@futurice.com>
2015-02-15 16:57:42 +0200

    added example structure for an app leveraging thematic.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+require('whatwg-fetch'); // for fetch polyfill
+var thematic = require('thematic');
+
+// initialize the map
+var map = new thematic.Thematic(document.getElementById('map'), {
+	imagePath: 'node_modules/thematic/build/images'
+});
+
+var dots = fetch('alko-markers.json')
+	.then(function(resp) { return resp.json(); })
+	.then(thematic.converters.flatToGeoJSON); // convert the data from the flat dot format to geojson
+
+// add the mapping module
+map.addModule('alkos', 
+	new thematic.modules.Dot({
+	    popupText: function(point) { 
+	        var props = point.properties;
+	        var url = 'http://www.alko.fi' + props.url;
+	        return '<a target="_blank" href="' + url + '">Alko ' + props.name + '</a><br>' + props.address + '<br>' + props.postalCode + ' ' + props.locality; 
+	    }
+	}).setData(dots) // and set the data
+);
